2c33d20b26a658848c21fa8afcd3d81bbde6660e
Trevahok gcp <vighneshss@gmail.com>
2019-05-20 17:08:07 +0000

    add docker file

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '1*95l0ui%zw$(!n#=*fl-)a69rb45)6=l8e$4rfgkfl@f*i*v%'
 
-''' local settings
+# local settings
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -37,7 +37,7 @@ DATABASES = {
 }
 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 '''
 
@@ -69,7 +69,7 @@ DATABASES['default'].update(db_from_env)
 
 
 # end production settings
-
+'''
 
 
 
